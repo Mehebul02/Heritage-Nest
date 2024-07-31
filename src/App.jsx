960c15a66be5e9128acb86eb43e87d990511ea62
Mehebul02 <mehebul2123@gmail.com>
2024-07-31 15:51:49 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1 className='text-2xl text-center text-red-600 font-semibold flex justify-center items-center h-screen'>bdCalling Task</h1>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-red-700">Home page</h1>
+        </div>
+    );
+};
+
+export default Home;
