338464cfee19e93c04cc4fde17eddc1485228cec
Mehebul02 <mehebul2123@gmail.com>
2024-07-31 22:28:00 +0600

    Property section complited

---
+++ src/component/Propertybuying/CustomProperty.jsx
@@ -0,0 +1,30 @@
+import CustomButton from "../CustomButton";
+import SectionTitle from "../SectionTitle";
+
+const CustomProperty = ({image_1,image_2,Icon,title,description}) => {
+    return (
+        <div className="flex flex-col lg:flex-row gap-6 justify-between items-center  my-10">
+        {/* images  */}
+        <div>
+           <div className="relative">
+           <img className=" w-[458px] rounded-md" src={image_1} alt="house" />
+          <div className="relative">
+          <img className="absolute -bottom-14 lg:-right-28 w-[300px] rounded-md" src={image_2} alt="house" />
+         <div className="absolute   right-28 lg:-right-6 -top-12 lg:-top-40 "> <Icon className="  text-4xl text-white rounded-full bg-primaryColor " /></div>
+          </div>
+           </div>
+
+        </div>
+        {/* text section  */}
+        <div className="max-w-[552px] space-y-10">
+            <SectionTitle title='Property buying'/>
+            <h1 className="text-3xl text-darkText font-bodyFont font-semibold">{title}</h1>
+        <p className="font-bodyFont">{description}</p>
+      <CustomButton/>
+        </div>
+
+    </div>
+    );
+};
+
+export default CustomProperty;
--- src/component/Propertybuying/PropertyBuying.jsx
+++ src/component/Propertybuying/PropertyBuying.jsx
@@ -5,22 +5,15 @@ import SectionTitle from "../SectionTitle";
 import services_house_1 from '../../assets/images/house/services_1.jpg'
 import services_house_2 from '../../assets/images/house/services_house_2.jpg'
 import { TiMediaPlayReverse } from "react-icons/ti";
+import CustomProperty from "./CustomProperty";
 const PropertyBuying = () => {
     return (
         <Container>
-            {/* Main div  */}
-            <div className="flex flex-col lg:flex-row gap-6 justify-between items-center  my-10">
-                {/* images  */}
-                <div>
-                   <div className="relative">
-                   <img className=" w-[458px] rounded-md" src={services_house_1} alt="house" />
-                  <div className="relative">
-                  <img className="absolute -bottom-14 lg:-right-28 w-[300px] rounded-md" src={services_house_2} alt="house" />
-                 <div className="absolute left-32 -top-12 lg:-right-6 lg:-top-40 "> <TiMediaPlayReverse className="  text-4xl text-white rounded-full bg-primaryColor " /></div>
-                  </div>
-                   </div>
-
-                </div>
+           <div className="space-y-12 lg:space-y-40">
+            {/* custom 1st section  */}
+           <CustomProperty image_1={services_house_1} image_2={services_house_2} Icon={TiMediaPlayReverse} title='Efficient and Transparent Home Buying Solutions' description='It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'/>
+             {/* 2nd section */}
+             <div className=" flex flex-col-reverse lg:flex-row  gap-7 space-x-44">
                 {/* text section  */}
                 <div className="max-w-[552px] space-y-10">
                     <SectionTitle title='Property buying'/>
@@ -28,8 +21,24 @@ const PropertyBuying = () => {
                 <p className="font-bodyFont">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p>
               <CustomButton/>
                 </div>
+                {/* images  */}
+                <div>
+                   <div className="relative">
+                   <img className=" lg:w-[458px] rounded-md" src={services_house_1} alt="house" />
+                  <div className="relative">
+                  <img className="absolute -bottom-14 lg:-right-28 w-[300px] rounded-md" src={services_house_2} alt="house" />
+                 <div className="absolute   right-28 lg:-right-6 -top-12 lg:-top-40 "> <TiMediaPlayReverse className="  text-4xl text-white rounded-full bg-primaryColor " /></div>
+                  </div>
+                   </div>
+
+                </div>
+                
 
             </div>
+            {/* 3rd custom section  */}
+            <CustomProperty image_1={services_house_1} image_2={services_house_2} Icon={TiMediaPlayReverse} title='Efficient and Transparent Home Buying Solutions' description='It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'/>
+           </div>
+          
         </Container>
     );
 };
