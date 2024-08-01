27ca5ace515015bacbe9278584052c3448d82a29
Mehebul02 <mehebul2123@gmail.com>
2024-08-01 23:21:06 +0600

    About frame complite

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -9,6 +9,7 @@ import viewMore from '../images/View more (1).png'
 import map from '../images/map.png'
 import aboutBanner from '../images/aboutBanner.png'
 import aboutFrame from '../images/aboutFrame.png'
+import aboutFrame1 from '../images/aboutFrame.jpg'
 
 
-export {logo,banner,cardImage1,detailsImg1,detailsImg2,detailsImg3,detailsImg4,viewMore,map,aboutBanner,aboutFrame}
+export {logo,banner,cardImage1,detailsImg1,detailsImg2,detailsImg3,detailsImg4,viewMore,map,aboutBanner,aboutFrame,aboutFrame1}
--- src/pages/About/AboutFrame.jsx
+++ src/pages/About/AboutFrame.jsx
@@ -1,4 +1,5 @@
-import { aboutFrame } from "../../assets/images";
+
+import { aboutFrame1 } from "../../assets/images";
 import Container from "../../component/Container";
 import SectionTitle from "../../component/SectionTitle";
 
@@ -6,10 +7,10 @@ const AboutFrame = () => {
     return (
         <Container>
             <div className="relative my-24">
-            <img className="w-[904px] h-[455px]  rounded-md" src={aboutFrame} alt="aboutFrame" />
-            <div className=" absolute z-50 bottom-0 right-0 w-[552px] h-[270px] bg-[#ECF5FF] p-6 rounded-md">
+            <img className="w-[904px] h-[455px]  rounded-md" src={aboutFrame1} alt="aboutFrame" />
+            <div className=" absolute z-50 bottom-0 right-0 lg:w-[552px] h-[270px] bg-[#ECF5FF] p-6 rounded-md">
                 <SectionTitle title='Our Story'/>
-                <h1 className="text-[36px] text-darkText font-montserrat font-bold">Efficient and Transparent Home Buying Solutions</h1>
+                <h1 className="lg:text-[36px] text-darkText font-montserrat font-bold">Efficient and Transparent Home Buying Solutions</h1>
                 <p className="text-[#6B7280] font-bodyFont mt-2">In the symphony of bustling markets and serene landscapes, Heritage-Nest was born—a vision to</p>
 
             </div>
