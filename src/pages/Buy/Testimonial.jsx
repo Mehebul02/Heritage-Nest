b326d5ffe23c13d565c57eb534cf6eece6209c88
Mehebul02 <mehebul2123@gmail.com>
2024-08-01 11:17:24 +0600

    testimonilas commplited

--- src/pages/Buy/Testimonial.jsx
+++ src/pages/Buy/Testimonial.jsx
@@ -1,16 +1,20 @@
-import React, { useRef, useState } from "react";
-// Import Swiper React components
+import React, { useEffect, useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-// import 'swiper/css/pagination';
-// import 'swiper/css/navigation';
-
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
-import { IoMdStarOutline } from "react-icons/io";
-
+import Container from "../../component/Container";
 const Testimonial = () => {
+    const [testimonials,setTestimonials] =useState([])
+    useEffect(()=>{
+        fetch('testimonial.json')
+        .then( res => res.json())
+        .then(data =>{
+            setTestimonials(data)
+        })
+    },[])
+    console.log(testimonials);
   return (
-    <div>
+    <Container>
       <div className="text-center mt-16 space-y-4 ">
         <h1 className="text-4xl text-darkText font-bodyFont font-semibold">
           Testimonial
@@ -36,38 +40,66 @@ const Testimonial = () => {
         className="mySwiper"
       >
        
-        <SwiperSlide>
-          <div className="max-w-[528px] mx-auto flex flex-col justify-center  p-6 shadow-md rounded-xl sm:px-12 bg-bgColor dark:bg-gray-50 dark:text-gray-800 my-10 ">
+          {
+            testimonials?.map(testimonial =>(
+                <SwiperSlide key={testimonial.id}>
+                <div  className="max-w-[528px] mx-auto flex flex-col justify-center  p-6 shadow-md rounded-xl sm:px-12 bg-bgColor dark:bg-gray-50 dark:text-gray-800 my-10 space-y-4">
             {/* rating  */}
             <div className="rating rating-md space-x-6 mx-auto">
-  <input type="radio" name="rating-7" className="mask mask-star-2 bg-designColor" />
-  <input type="radio"name="rating-7"className="mask mask-star-2 bg-designColor"/>
-  <input type="radio" name="rating-7" className="mask mask-star-2 bg-designColor" />
-  <input type="radio" name="rating-7" className="mask mask-star-2 bg-designColor" />
-  <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-300" />
-  
-</div>
-{/* text  */}
-<p className="text-[#818181] text-center font-bodyFont">"The level of security provided by CypherPlay is unmatched. I feel confident using my card for both everyday purchases and travel. It's the peace of mind I was looking for."</p>
-          {/* person  */}
-     
-      <div className="avatar">
-  <div className="w-12 mx-auto rounded-full">
-    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-    
-  </div>
-</div>
+              <input
+                type="radio"
+                name="rating-7"
+                className="mask mask-star-2 bg-designColor"
+              />
+              <input
+                type="radio"
+                name="rating-7"
+                className="mask mask-star-2 bg-designColor"
+              />
+              <input
+                type="radio"
+                name="rating-7"
+                className="mask mask-star-2 bg-designColor"
+              />
+              <input
+                type="radio"
+                name="rating-7"
+                className="mask mask-star-2 bg-designColor"
+              />
+              <input
+                type="radio"
+                name="rating-7"
+                className="mask mask-star-2 bg-orange-300"
+              />
+            </div>
+            {/* text  */}
+            <p className="text-[#818181] text-center font-bodyFont">
+              {testimonial.description}
+            </p>
+            {/* person  */}
 
-      
-      <div>
-        <h1 className="text-[20px] text-center font-montserrat font-bold">Tony Stark</h1>
-        <p className="text-[16px] text-[#6B7280] text-center">Marketing manager, XYZ</p>
-      </div>
+            <div className="avatar">
+              <div className="w-12 mx-auto rounded-full">
+                <img src={testimonial.img} />
+              </div>
+            </div>
+
+            <div>
+              <h1 className="text-[20px] text-center font-montserrat font-bold">
+               {testimonial.name}
+              </h1>
+              <p className="text-[16px] text-[#6B7280] text-center">
+                {testimonial.title}
+              </p>
+            </div>
           </div>
-        </SwiperSlide>
-        
+          </SwiperSlide>
+            ))
+          }
+       
+       
       </Swiper>
-    </div>
+    </Container>
   );
 };
 
